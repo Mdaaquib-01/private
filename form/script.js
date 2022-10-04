93b743492a514bd3d77290a24d688160f25abae8
aaquib anaikar <aaquib0000@gmail.com>
2022-10-04 21:25:09 +0530

    new commit

--- form/script.js
+++ form/script.js
@@ -1,16 +1,12 @@
 mainform = document.querySelector('.form1');
 popupform = document.querySelector('.form2');
 form1Inputs = document.querySelectorAll('.form1 input');
-form2Inputs = document.querySelectorAll('.form2 input');
-nameval = document.querySelector('.form1 #Name')
-emailval = document.querySelector('.form1 #Email')
-ageval = document.querySelector('.form1 #Age')
-addressval = document.querySelector('.form1 #Address')
-phoneval = document.querySelector('.form1 #Phone')
+form1Label = document.querySelectorAll('.form1 label');
+form2Inputs = document.querySelector('.form2 input');
+form2Label = document.querySelector('.form2 #label');
 cross = document.querySelector('.cross');
-if (popupform.classList.contains('hide')) {
-    console.log("hello");
-}
+inputLabel = document.getElementById('label');
+inputValue = document.getElementById('value');
 
 function hide() {
     popupform.classList.remove('hide');
@@ -20,44 +16,54 @@ function show(){
     popupform.classList.add('hide');
     mainform.classList.remove('form1move');
 }
-form2Inputs.forEach(ele => {
-    ele.addEventListener('keyup', match);
-    ele.addEventListener('focus', match);
-    ele.addEventListener('blur', () => {
-        form1Inputs.forEach(e => {
-            e.style.backgroundColor = "#e1cccc";
-            e.style.color = "#111"
-        })
+form2Label.addEventListener('keyup',match);
+inputValue.addEventListener('blur', () => {
+    inputLabel.value=null;
+    inputValue.value=null;
+    form1Inputs.forEach(e => {
+        e.style.backgroundColor = "#e1cccc";
+        e.style.color = "#111"
+        e.style.transform = "scale(1)"
+    })
+})
+function match(event) {
+    val = event.target.value.toLowerCase();
+    form1Label.forEach(ele => {
+        lowerEle = ele.textContent.toLowerCase();
+        if (val === lowerEle) {
+            parent = ele.nextElementSibling;
+            inp = parent.children[0];
+            console.log(inp);
+            colorFocus(inp);
+            
+            inputValue.addEventListener('keyup',(e)=>{
+                if (val === ele.textContent.toLowerCase()) {
+                    inp.value = e.target.value;
+                }
+               
+            })
+            
+            
+        }
     });
 
-});
-function match(event) {
-
-    val = event.target.id;
-    console.log(val)
-    if (val === 'Name') {
-        nameval.value = event.target.value;
-        colorFocus(nameval);
-    }
-    else if (val === 'Email') {
-        emailval.value = event.target.value;
-        colorFocus(emailval);
-    }
-    else if (val === 'Age') {
-        ageval.value = event.target.value;
-        colorFocus(ageval);
-    }
-    else if (val === 'Address') {
-        addressval.value = event.target.value;
-        colorFocus(addressval);
-    }
-    else if (val === 'Phone') {
-        phoneval.value = event.target.value;
-        colorFocus(phoneval);
-    }
 }
 
 function colorFocus(e) {
     e.style.backgroundColor = "#6ac86a";
     e.style.color = "#fff"
+    e.style.transform = "scale(1.1)"
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
